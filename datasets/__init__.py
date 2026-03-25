dc93421a099aeccaa53ecddc3901f9311a688580
wuweidong <wuweidong@58.com>
2026-03-25 19:55:07 +0800

    fix

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,8 +4,6 @@ from .poly_data import build as build_poly
 
 
 def build_dataset(image_set, args):
-    if args.semantic_classes > 0:
-        assert args.dataset_name == 'stru3d', "Semantically-rich floorplans only support Structured3D"
-    if args.dataset_name == 'stru3d' or args.dataset_name == 'scenecad':
+    if args.dataset_name in ['stru3d', 'scenecad', 'custom']:
         return build_poly(image_set, args)
     raise ValueError(f'dataset {args.dataset_name} not supported')
