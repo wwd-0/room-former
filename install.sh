3371b9aeded0a22b15297139daca1d1fdca80894
wuweidong <wuweidong@58.com>
2026-03-26 17:33:39 +0800

    fix

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ bash make.sh
 cd ../../diff_ras
 python setup.py build develop
 cd ..
-python main.py  --dataset_root /content/dataset_v1 --epochs 500 --batch_size 1 --lr 2e-4 --lr_drop 400 --num_queries 800 --num_polys 20 --use_pano --backproject_pano --output_dir ../output
+python main.py  --dataset_root /content/dataset_v1 --epochs 500 --batch_size 1 --lr 2e-4 --lr_drop 400 --num_queries 800 --num_polys 20 --use_pano --pano_backproject_bias --output_dir ../output
